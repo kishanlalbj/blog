35d86f4d206c9e21172849fa873f1ecf5b13bb15
kishanlalbj <kishanlalbj@gmail.com>
2019-12-05 04:45:18 +0530

    attempt 2 of page reload issue

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -46,10 +46,21 @@ app.use(function(req, res, next) {
   next();
 });
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client", "build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
