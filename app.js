2ebcb4148b9569aa32e84d4fabe2f83d9ba7acf8
kishanlalbj <kishanlalbj@gmail.com>
2019-12-05 04:32:48 +0530

    fix cannot get issue

--- app.js
+++ app.js
@@ -48,9 +48,9 @@ app.use(function(req, res, next) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build"));
+  });
 }
 
 // app.get("*", (req, res) => {
